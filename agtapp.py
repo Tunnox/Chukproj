4380da5f736eff97473b2e9a20220e3bda3576d8
Tunnox <chukwuemekaumunna@gmail.com>
2024-12-07 23:47:55 +0000

    Update agtapp.py

--- agtapp.py
+++ agtapp.py
@@ -11,8 +11,8 @@ def get_db_connection():
         user='ADMIN', 
         password='vUcW96AC.gd.d4y', 
         dsn='a1z2qbbh602iu73y_medium', 
-        config_dir="C:\\ChukProj\\flask_proj\\config_dir",
-        wallet_location="C:\\ChukProj\\flask_proj\\config_dir", 
+        config_dir="C:\ChukProj\flask_proj\config_dir",
+        wallet_location="C:\ChukProj\flask_proj\config_dir", 
         wallet_password='gHpt468/Chuk.VGA.Lli.ps'
     )
 

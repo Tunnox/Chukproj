5e6fd6c6030e8be5575775a79328589bf840d443
Tunnox <chukwuemekaumunna@gmail.com>
2025-06-07 21:23:39 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -2,14 +2,37 @@ from flask import Flask, render_template, request, jsonify, redirect, url_for, f
 import psycopg2
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Replace with your secret key
+app.secret_key = 'Chuk_secret_key'  # Replace with your secret key
 
 # Configure your PostgreSQL database connection here
 connection = psycopg2.connect(dbname='AGT', user='postgres', password='pgsqtk116chuk95', host='chukspace.ctiuisa62ks5.eu-north-1.rds.amazonaws.com', port='5432')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('Main.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    login_username = data['loginUsername']
+    login_password = data['loginPassword']
+
+    cur = connection.cursor()
+    try:
+        cur.execute(
+            'SELECT * FROM "public"."AGT_USER_LOGIN" WHERE ("USERNAME" = %s OR "EMAIL" = %s) AND "PASSWORD" = %s;', 
+            (login_username, login_username, login_password)
+        )
+        user = cur.fetchone()
+        
+        if user:
+            return jsonify({"message": "Login successful!", "success": True}), 200
+        else:
+            return jsonify({"message": "Invalid credentials, please create an account."}), 401
+    except Exception as e:
+        return jsonify({"message": str(e)}), 400
+    finally:
+        cur.close()
 
 @app.route('/search', methods=['POST'])
 def search():
@@ -45,7 +68,6 @@ def search():
     
     return render_template('index.html', results=results)
 
-
 @app.route('/insert', methods=['GET','POST'])
 def insert():
     if request.method == 'POST':
@@ -69,7 +91,7 @@ def insert():
         cursor.execute("""
             INSERT INTO "public"."data_records" ("title", "first_name", "last_name", "age", "gender", "date_of_birth", "address", "email", "mobile_number", "status", "department", "relationship_status", "employement_status", "consent")
             VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
-        """, (title, first_name, last_name, age, gender,date_of_birth, address, email, mobile_number, status,department, relationship_status, employment_status, consent))
+        """, (title, first_name, last_name, age, gender, date_of_birth, address, email, mobile_number, status, department, relationship_status, employment_status, consent))
         connection.commit()  # Don't forget to commit the transaction!
         cursor.close()
     
@@ -97,5 +119,4 @@ def insert_attendance():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
     
